523b0b9a8cda6ca0d4061ecdd1cd6a3b3b66cfc9
DVP-F <carnx@duck.com>
2025-06-23 13:29:08 +0300

    number guessing w/o randomization

--- projects/pre-planned/guess/src/main.rs
+++ projects/pre-planned/guess/src/main.rs
@@ -1,3 +1,32 @@
+use std::{io::{self, Write}};
+
 fn main() {
-    println!("Hello, world!");
+	const NUMBER: u32 = 12;
+	let mut num: u32 = 0;
+
+	let mut checking = true;
+	while checking{
+		print!("Guess a number or type 'exit'/^C to quit: "); 
+		io::stdout().flush().unwrap();
+		let mut input = String::new();
+		io::stdin().read_line(&mut input).expect("Failed to read input");
+
+		for ch in input.chars() {
+			if let Some(digit) = ch.to_digit(10){
+				num = num * 10 + digit as u32;
+			}
+		}
+
+		if input.to_lowercase() == "exit" {checking = false}
+		else if num > NUMBER {
+			println!("Too high~~")
+		}
+		else if num < NUMBER {
+			println!("Too low bro")
+		}
+		else if num == NUMBER {
+			println!("Congratulations! The number was {}", NUMBER);
+			checking = false;
+		}
+	}
 }
