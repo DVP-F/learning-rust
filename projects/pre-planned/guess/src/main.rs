1c368a0821395373771906b324a9d27c059addc6
DVP-F <carnx@duck.com>
2025-06-25 02:39:07 +0300

    finished :3

--- projects/pre-planned/guess/src/main.rs
+++ projects/pre-planned/guess/src/main.rs
@@ -1,32 +1,37 @@
 use std::{io::{self, Write}};
+use rand::Rng;
 
 fn main() {
-	const NUMBER: u32 = 12;
-	let mut num: u32 = 0;
+	let mut rng = rand::rng();
+	let n: u32 = rng.random();
+	let number = u64::from(n);
+
+	let mut num: u64 = 0;
 
 	let mut checking = true;
 	while checking{
-		print!("Guess a number or type 'exit'/^C to quit: "); 
+		print!("Guess a number or type 'exit'/^C to quit: ");
 		io::stdout().flush().unwrap();
 		let mut input = String::new();
 		io::stdin().read_line(&mut input).expect("Failed to read input");
 
 		for ch in input.chars() {
 			if let Some(digit) = ch.to_digit(10){
-				num = num * 10 + digit as u32;
+				num = num * 10 + digit as u64;
 			}
 		}
 
 		if input.to_lowercase() == "exit" {checking = false}
-		else if num > NUMBER {
+		else if num > number {
 			println!("Too high~~")
 		}
-		else if num < NUMBER {
+		else if num < number {
 			println!("Too low bro")
 		}
-		else if num == NUMBER {
-			println!("Congratulations! The number was {}", NUMBER);
+		else if num == number {
+			println!("Congratulations! The number was {}", number);
 			checking = false;
 		}
+		num = 0;
 	}
 }
